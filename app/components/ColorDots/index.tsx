f317d0acde7d2932869f409f341a68458a702b6f
Shadi <ra.rahdari@gmail.com>
2024-08-18 23:53:28 +0300

    PUR-17: product cart added

---
+++ app/components/ColorDots/index.tsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+export const ColorDots = ({}) => {
+  return (
+    <div className="es-colors">
+      <div className="es-color es-color-1" />
+      <div className="es-color es-color-2" />
+      <div className="es-color es-color-3" />
+      <div className="es-color es-color-4" />
+    </div>
+  );
+};
