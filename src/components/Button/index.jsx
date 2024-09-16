3fd756d325bf082f71ea77c9518f52ea6aff6ef6
Oleksadra Korkh <sashkacream0@gmail.com>
2024-09-16 14:42:51 +0300

    refactor: change styles for form and button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,18 +5,18 @@ import PropTypes from 'prop-types';
 
 export const Button = ({ size, color, type = 'button', children }) => {
   const btnSizes = {
-    filter: 'px-6 py-2 w-40 md:w-32',
-    seeResults: 'px-6 py-3 w-36 md:w-5/12',
-    mostPopular: 'px-6 py-2 w-40',
-    order: 'px-6 py-3 w-36',
-    form: 'px-6 py-3 w-full lg:w-40',
+    filter: 'w-40 md:w-32',
+    seeResults: 'w-36 md:w-5/12',
+    mostPopular: 'w-40',
+    order: 'w-36',
+    form: 'w-full lg:w-40',
   };
 
   const btnColors = {
     brightOutlined:
       'border border-secondary-bright text-secondary-bright hover:bg-secondary-light ',
     regular: 'text-white bg-main-regular text-white hover:border-main-regular ',
-    dark: 'text-white font-medium bg-secondary-dark hover:border-secondary-light ',
+    dark: 'text-white font-medium bg-secondary-dark hover:scale-105',
     darkOutlined:
       'border border-secondary-dark text-secondary-dark font-medium hover:border-secondary-light',
   };
@@ -25,7 +25,7 @@ export const Button = ({ size, color, type = 'button', children }) => {
 
   return (
     <button
-      className={`font-medium rounded-full text-base ${btnSizeClasses} ${btnColorClasses}`}
+      className={`button px-6 py-4 font-medium text-base ${btnSizeClasses} ${btnColorClasses}`}
       type={type}>
       {children}
     </button>
--- src/components/LeadForm/index.jsx
+++ src/components/LeadForm/index.jsx
@@ -23,25 +23,27 @@ const LeadForm = () => {
   });
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white shadow-lg p-8 rounded-md w-full max-w-md md:max-w-lg lg:max-w-2xl flex flex-col lg:flex-row">
+    <div className="flex justify-center min-h-screen">
+      <div className="p-3 w-full lg:max-w-4xl flex flex-col lg:flex-row">
         <div
           className="hidden lg:block bg-cover bg-no-repeat w-72 h-full"
           style={{ backgroundImage: 'url("your-image-url.jpg")' }}>
           <img src={formBackground} alt="purple form background" />
         </div>
-        <div className="flex-1 p-4">
-          <div className="flex justify-between items-start mb-6">
-            <h1 className="text-left font-bold text-2xl lg:text-4xl leading-9 lg:leading-tight">
+        <div className="flex-1 p-1 lg:px-12 lg:py-2.5">
+          <div className="flex justify-between mb-6 mt-1.5">
+            <h1 className="font-bold text-3xl leading-9 lg:leading-tight">
               Finalise Your Order
             </h1>
             <button
               aria-label="Close form"
-              className="text-gray-500 hover:text-gray-800"></button>
-            <FontAwesomeIcon icon={faXmark} />
+              className="button text-3xl relative -top-3 md:static lg:relative lg:-top-4 hover:scale-125">
+              <FontAwesomeIcon icon={faXmark} />
+            </button>
           </div>
-          <p className="text-left font-normal text-base lg:text-lg mb-4 leading-6">
-            Please fill in the form below to place your order.
+          <p className="text-left font-normal text-base lg:text-lg mb-5 leading-6">
+            Please leave your contact details below. We’ll contact you very
+            shortly to finalise your order.
           </p>
 
           <Formik
@@ -55,17 +57,17 @@ const LeadForm = () => {
                 <div>
                   <label
                     htmlFor="name"
-                    className="block text-sm font-medium leading-5">
+                    className="block text-sm mb-1.5 font-medium leading-5">
                     * Name and Surname
                   </label>
                   <Field
                     id="name"
                     name="name"
-                    className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-600"
+                    className="w-full px-3 pt-2 pb-3 border rounded-sm border-neutral-500 focus:outline-none focus:ring-1 focus:ring-main"
                   />
                   <ErrorMessage name="name">
                     {(msg) => (
-                      <div className="text-red-500 text-sm mt-1 flex items-center gap-2">
+                      <div className="text-border-error text-sm mt-1 flex items-center gap-2">
                         <FontAwesomeIcon icon={faTriangleExclamation} /> {msg}
                       </div>
                     )}
@@ -74,18 +76,18 @@ const LeadForm = () => {
                 <div>
                   <label
                     htmlFor="email"
-                    className="block text-sm font-medium leading-5">
+                    className="block text-sm mb-1.5 font-medium leading-5">
                     * Email address
                   </label>
                   <Field
                     id="email"
                     name="email"
                     type="email"
-                    className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-600"
+                    className="w-full px-3 pt-2 pb-3 border rounded-sm border-neutral-500 focus:outline-none focus:ring-1 focus:ring-main invalid:border-border-error"
                   />
                   <ErrorMessage name="email">
                     {(msg) => (
-                      <div className="text-red-500 text-sm mt-1 flex items-center gap-2">
+                      <div className="text-border-error text-sm mt-1 flex items-center gap-2">
                         <FontAwesomeIcon icon={faTriangleExclamation} /> {msg}
                       </div>
                     )}
@@ -94,26 +96,28 @@ const LeadForm = () => {
                 <div>
                   <label
                     htmlFor="phone"
-                    className="block text-sm font-medium leading-5">
+                    className="block text-sm mb-1.5 font-medium leading-5">
                     * Phone number
                   </label>
                   <Field
                     id="phone"
                     name="phone"
                     type="tel"
-                    className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-600"
+                    className="w-full px-3 pt-2 pb-3  border rounded-sm border-neutral-500 focus:outline-none focus:ring-1 focus:ring-main"
                   />
                   <ErrorMessage name="phone">
                     {(msg) => (
-                      <div className="text-red-500 text-sm mt-1 flex items-center gap-2">
+                      <div className="text-border-error text-sm mt-1 flex items-center gap-2">
                         <FontAwesomeIcon icon={faTriangleExclamation} /> {msg}
                       </div>
                     )}
                   </ErrorMessage>
                 </div>
-                <Button type="submit" color={'darkOutlined'} size={'filter'}>
-                  Place an order
-                </Button>
+                <div className="mt-6">
+                  <Button type="submit" color={'dark'} size={'form'}>
+                    Place an order
+                  </Button>
+                </div>
               </Form>
             )}
           </Formik>
