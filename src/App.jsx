d847a55547897fd3ee02d2b88b5c00793d67b9bc
Oleksadra Korkh <sashkacream0@gmail.com>
2024-09-20 15:06:29 +0300

    merge: merge main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD:src/App.jsx
-=======
 import './tailwind.css';
 import { ProductsList } from './components/ProductsList/index.jsx';
->>>>>>> main:client/src/App.jsx
 import Home from './pages/Home/index.jsx';
 import Header from './components/Header/index.jsx';
 
@@ -12,12 +9,8 @@ function App() {
     <div className="min-h-screen font-main">
       <Header />
       <Home />
-<<<<<<< HEAD:src/App.jsx
-    </div>
-=======
       <ProductsList />
-    </>
->>>>>>> main:client/src/App.jsx
+    </div>
   );
 }
 
