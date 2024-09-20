89abb6992de0ab049c29dfdd0d0518676cead197
Oleksadra Korkh <sashkacream0@gmail.com>
2024-09-20 15:03:29 +0300

    refactor: delete client folder

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './tailwind.css';
+import Home from '../src/pages/Home/index.jsx';
+/* import { ProductsList } from '../components/ProductsList/index.jsx'; */
+
+
+function App() {
+  return (
+    <>
+      <Home />
+      {/* <ProductsList /> */}
+    </>
+  );
+}
+
+export default App;
