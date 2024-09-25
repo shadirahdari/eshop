d44dd8ba4992d3cf299b6447546758ce70b8bf4d
Oleksadra Korkh <sashkacream0@gmail.com>
2024-09-25 22:58:47 +0300

    feat: implement cloudinary for storing img

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -13,6 +13,10 @@ import icons from '../../assets/svg/icons.svg';
 
 export const ProductCard = ({ product }) => {
   const {openModal} = useContext(ModalContext);
+  const imageUrl = (productImg) => {
+    return `https://res.cloudinary.com/dewhrjfex/image/upload/${productImg}`;
+  }
+
   return (
     <div className="es-product-card">
       <div className="es-product-card-main">
