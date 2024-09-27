512ab252457778ab2fe7c900d2a80f26812d5af4
Oleksadra Korkh <sashkacream0@gmail.com>
2024-09-27 11:29:02 +0300

    fix: fix button styles

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -13,15 +13,17 @@ export const Button = ({
   const btnSizes = {
     filter: 'w-full md:w-32',
     seeResults: 'w-36 md:w-5/12',
-    mostPopular: 'w-40',
+    mostPopular: 'w-full md:w-auto',
     order: 'w-36',
-    form: 'w-full lg:w-40 lg:mt-8',
+    form: 'w-full lg:w-40 lg:mt-8 py-4',
+    filtersClose: 'w-full h-12',
+    filtersResults: 'w-full mr-2',
   };
 
   const btnColors = {
     brightOutlined: `${className}  border border-secondary-bright text-secondary-bright hover:bg-secondary-light  focus:bg-secondary-light`,
     regular: ` ${className} py-4 text-white bg-main hover:border-main-regular `,
-    dark: 'py-4 text-white bg-secondary-dark hover:scale-[1.02] border-secondary-dark',
+    dark: 'text-white bg-secondary-dark border-secondary-dark',
     darkOutlined:
       'border border-secondary-dark text-secondary-dark font-medium hover:border-secondary-light',
   };
@@ -31,7 +33,7 @@ export const Button = ({
 
   return (
     <button
-      className={`button font-medium text-base ${btnSizeClasses} ${btnColorClasses} focus:border-1`}
+      className={` button font-medium text-base ${btnSizeClasses} ${btnColorClasses} focus:border-1`}
       type={type}
       onClick={onClick}>
       {children}
