82a610daeb7f055e13ce926edf342d7f7a188fda
Shadi <ra.rahdari@gmail.com>
2024-10-02 01:32:33 +0300

    fix: updated price to monthly payments

--- src/components/MonthlyPayment/index.jsx
+++ src/components/MonthlyPayment/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export const MonthlyPayment = ({ price = 0 }) => {
   return (
     <div className="font-helvetica font-bold text-[16px] leading-[24px] text-left w-[143px] h-[24px] gap-1  flex flex-1">
-      {price} Eur/Month
+      {Math.ceil(price / 12)} Eur/Month
     </div>
   );
 };
