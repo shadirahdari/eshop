c477bd5bc2a1fd13900d398fbb0e3f07584ba70a
Shadi <ra.rahdari@gmail.com>
2024-10-02 01:55:50 +0300

    fix: Updated form ui to fill 90% of the screen. Before it was filling 60% of scrollable content which was several screens

--- src/components/ModalOrder/index.jsx
+++ src/components/ModalOrder/index.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useRef, useState} from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { useClickOutside } from '../../hooks/useClickOutside.jsx';
 import Modal from 'react-modal';
 import { ModalContext } from '../../store/modal-context.jsx';
@@ -10,47 +10,47 @@ import formBackground from '../../assets/images/form_background.png'
 Modal.setAppElement('#root');
 
 export function ModalOrder() {
-    const modalRef = useRef(null);
-    const { isModalOpen, closeModal, isFormSubmitted, setIsFormSubmitted } = useContext(ModalContext);
-    const [isSuccess, setIsSuccess] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+  const modalRef = useRef(null);
+  const { isModalOpen, closeModal, isFormSubmitted, setIsFormSubmitted } = useContext(ModalContext);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useClickOutside(modalRef,  closeModal);
+  useClickOutside(modalRef, closeModal);
 
-    const renderContent = () => {
-      /*  if (isLoading) {
-            return <p>Loading...</p>;
-        }
+  const renderContent = () => {
+    /*  if (isLoading) {
+          return <p>Loading...</p>;
+      }
 */
-        if (!isFormSubmitted) {
-            return (
-                <OrderFormContent
-                    onFormSubmit={(success) => {
-                        setIsFormSubmitted(true);
-                        setIsSuccess(success);
-                        setIsLoading(false);
-                    }}
-                    setIsLoading={setIsLoading}
-                    isLoading={isLoading}
-                />
-            );
-        }
+    if (!isFormSubmitted) {
+      return (
+        <OrderFormContent
+          onFormSubmit={(success) => {
+            setIsFormSubmitted(true);
+            setIsSuccess(success);
+            setIsLoading(false);
+          }}
+          setIsLoading={setIsLoading}
+          isLoading={isLoading}
+        />
+      );
+    }
 
-        return <StatusOfOrder isSuccess={isSuccess} closeModal={closeModal} onRetry={() => setIsFormSubmitted(false)} />
-    };
+    return <StatusOfOrder isSuccess={isSuccess} closeModal={closeModal} onRetry={() => setIsFormSubmitted(false)} />
+  };
   return (
     <Modal
       isOpen={isModalOpen}
       onRequestClose={closeModal}
-      className="bg-white w-full relative lg:max-w-4xl z-10 mx-auto transition-opacity duration-300 ease-in-out opacity-100"
-      overlayClassName="flex justify-center fixed inset-0 top-0 lg:bg-slate-800 lg:bg-opacity-35 items-center transition-opacity duration-300 ease-in-out "
+      className="relative bg-white w-full relative lg:h-[90vh] lg:max-w-4xl z-10 mx-auto transition-opacity duration-300 ease-in-out opacity-100"
+      overlayClassName="flex justify-center fixed inset-0 top-0 lg:bg-slate-800 lg:bg-opacity-90 items-center transition-opacity duration-300 ease-in-out "
       role="dialog"
       aria-labelledby="modalTitle"
-      >
+    >
       <div
-        className="p-3 lg:p-0 container h-screen lg:h-3/5 flex justify-center"
+        className="lg:p-0 container h-screen flex justify-center"
         ref={modalRef}>
-        <div className="hidden lg:block bg-cover bg-no-repeat w-72 overflow-hidden ">
+        <div className="hidden lg:block bg-cover bg-no-repeat lg:h-[90vh]  w-72 overflow-hidden ">
           <img
             src={formBackground}
             alt="purple form background"
@@ -58,8 +58,8 @@ export function ModalOrder() {
           />
         </div>
         <div className="flex-1 p-1 lg:px-12 lg:py-2.5">
-          <FormTitle closeModal={closeModal}/>
-            {renderContent()}
+          <FormTitle closeModal={closeModal} />
+          {renderContent()}
         </div>
       </div>
     </Modal>
