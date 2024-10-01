ee705fe1decae48143b6c8f10c341e7446411f01
Shadi <ra.rahdari@gmail.com>
2024-10-02 00:34:07 +0300

    fix: color name removed from product colors row

--- src/components/ColorDots/index.jsx
+++ src/components/ColorDots/index.jsx
@@ -5,7 +5,6 @@ export const ColorDots = ({ color = "" }) => {
   return (
     <div className="flex gap-2 mt-7 items-center text-sm">
       <span title={color} style={{ backgroundColor: color }} className={`w-3 h-3 rounded-full border border-[#00000054]`} />
-      {color}
     </div>
   );
 };
